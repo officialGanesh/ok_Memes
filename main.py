e5ee631481e64b50704e8de1f0a727281d77d452
Ganesh <ganuofficial2795@gmail.com>
2021-06-02 14:55:42 +0530

    json file and meme link

--- main.py
+++ main.py
@@ -15,9 +15,25 @@ def Basic_setup(url):
 
     return r
 
+def json_data(data):
+    '''Writing/Updating the json file'''
 
+    with open('Results/data.json','w') as f:
+        f.write(json.dumps(data,indent=2))
+
+    # Displaying Some Results In Terminal 
+
+    with open('Results/data.json','r') as f:
+        py_data = json.load(f)
+        # pprint(py_data)
+
+        print('Meme Results 😄')
+        print(f'Title --> {py_data["title"]}')
+        print(f"Meme-link --> {py_data['url']}")
 
 if __name__ == "__main__":
 
     req = Basic_setup(url=END_POINT)
+    json_data(data=req.json())
+
     print('Code Completed 🔥')
